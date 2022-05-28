eb992afce4ac7b5422fa8b99da93081a06e83792
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-05-28 14:25:29 +0700

    Context ID functionality  (#1)
    * ecid functionality - wip
    
    * Update profiler.h
    
    Remove leftover
    
    * Move ecid to ExecutionEvent for now...
    
    * ECID for other modes
    
    * ECID for other modes
    
    * Controll of tls model
    
    * Swap to pthread_set/get
    
    * Swap to pthread_set/get
    
    * Changes after PR comments
    
    * single malloc
    
    * PR comments
    
    * PR comments
    
    * PR comments
    
    * PR comments
    
    * Javadoc
    
    Co-authored-by: Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
    Co-authored-by: Krzysztof Ślusarski <57556371+krzysztofslusarski@users.noreply.github.com>
    Co-authored-by: Krzysztof Ślusarski <pasqal@gmail.com>

--- src/flightRecorder.cpp
+++ src/flightRecorder.cpp
@@ -1075,6 +1075,7 @@ class Recording {
         buf->putVar32(tid);
         buf->putVar32(call_trace_id);
         buf->putVar32(event->_thread_state);
+        buf->putVar64(Profiler::instance()->getContextId());
         buf->put8(start, buf->offset() - start);
     }
 
@@ -1087,6 +1088,7 @@ class Recording {
         buf->putVar32(event->_class_id);
         buf->putVar64(event->_instance_size);
         buf->putVar64(event->_total_size);
+        buf->putVar64(Profiler::instance()->getContextId());
         buf->put8(start, buf->offset() - start);
     }
 
@@ -1098,6 +1100,7 @@ class Recording {
         buf->putVar32(call_trace_id);
         buf->putVar32(event->_class_id);
         buf->putVar64(event->_total_size);
+        buf->putVar64(Profiler::instance()->getContextId());
         buf->put8(start, buf->offset() - start);
     }
 
@@ -1111,6 +1114,7 @@ class Recording {
         buf->putVar32(event->_class_id);
         buf->put8(0);
         buf->putVar64(event->_address);
+        buf->putVar64(Profiler::instance()->getContextId());
         buf->put8(start, buf->offset() - start);
     }
 
